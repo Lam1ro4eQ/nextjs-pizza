9df9fe423fe869ba5bf79827172a4a391cc3d14b
Lam1ro4eQ <alsajgushev@gmail.com>
2024-12-20 19:26:54 +0300

    commit)

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
-import { Categories, Container, Title } from "@/components/shared";
+import { Container, Title, TopBar } from "@/components/shared";
+
 
 export default function ProductPage() {
     return (
     <>
         <Container className='mt-10'>
             <Title text='Все пиццы' size="lg" className="font-extrabold"/>
-            <Categories/>
         </Container>   
-    
-    
-    
+        <TopBar/>
     </>
     )
 }
--- components/shared/sort-popup.tsx
+++ components/shared/sort-popup.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/lib/utils";
+import { ArrowUpDown } from "lucide-react";
 import React from "react";
 
 interface Props {
@@ -12,6 +13,10 @@ export const SortPopup: React.FC<Props> = ({ className }) => {
         "inline-flex items-center gap-1 bg-gray-50 px-5 h-[52px] rounded-2xl cursor-pointer",
         className
       )}
-    ></div>
+    >
+        <ArrowUpDown size={16}/>
+        <b>Сортировка</b>
+        <b className="text-primary">популярное</b>
+    </div>
   );
 };
