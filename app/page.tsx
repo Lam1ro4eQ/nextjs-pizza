8b233380a68fbade28b98cc72998482c0207420f
Lam1ro4eQ <alsajgushev@gmail.com>
2024-12-22 20:10:21 +0300

    filters

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,29 @@
-import { Container, Title, TopBar } from "@/components/shared";
-
+import { Container, Filters, Title, TopBar } from "@/components/shared";
 
 export default function ProductPage() {
-    return (
+  return (
     <>
-        <Container className='mt-10'>
-            <Title text='Все пиццы' size="lg" className="font-extrabold"/>
-        </Container>   
-        <TopBar/>
+      <Container className="mt-10">
+        <Title text="Все пиццы" size="lg" className="font-extrabold" />
+      </Container>
+      <TopBar />
+
+      <Container className="pb-14 mt-10">
+        <div className="flex gap-[60px]">
+          {/* фильтрация */}
+          <div className="w-[250px]">
+            <Filters />
+          </div>
+          {/*список товаров */}
+          <div className="flex-1">
+            <div className="flex flex-col gap-16">
+                список товаров
+              {/* <ProductsGroupList title="Пиццы" items={[1, 2, 3, 4, 5]} />
+              <ProductsGroupList title="Комбо" items={[1, 2, 3, 4, 5]} /> */}
+            </div>
+          </div>
+        </div>
+      </Container>
     </>
-    )
+  );
 }
